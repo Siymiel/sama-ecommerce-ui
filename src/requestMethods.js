c6aa4010ed80dfbb6f9d7bc787b6d4cb52052887
Samuel Kinuthia <51239970+Siymiel@users.noreply.github.com>
2022-11-12 14:26:34 +0300

    Updates

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const BASE_URL = "http://localhost:5000/api/v1";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNGIwMDg2ODJmMzZlYjQzNTE5NDk3OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2NjQyODQwOSwiZXhwIjoxNjY2Njg3NjA5fQ.HWEXt24ssQDDzFFCsskRzfk4W4zCihc-pYOkjsDUkOI";
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.accessToken
+// console.log(TOKEN)
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
