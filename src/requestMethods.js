ce2d7234f18a1b2c75c548d5415b8a106c30e426
Samuel Kinuthia <51239970+Siymiel@users.noreply.github.com>
2022-11-28 15:04:59 +0300

    Added fake token

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = "http://localhost:5000/api/v1";
-// const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNGIwMDg2ODJmMzZlYjQzNTE5NDk3OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2NjQyODQwOSwiZXhwIjoxNjY2Njg3NjA5fQ.HWEXt24ssQDDzFFCsskRzfk4W4zCihc-pYOkjsDUkOI";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.accessToken
+const fakeToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJpZCI6IjYzNGIwMDg2ODJmMzZlYjQzNTE5NDk3OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2NjQyODQwOSwiZXhwIjoxNjY2Njg3NjA5fQ.HWEXt24ssQDDzFFCsskRzfk4W4zCihc-pYOkjsDUkOI";
+const TOKEN = localStorage.getItem("persist:root") ? JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.accessToken : fakeToken
 // console.log(TOKEN)
 
 export const publicRequest = axios.create({
